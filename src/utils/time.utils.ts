0f35517629ef4d14d3f36a7f998c009accfd550a
CatalinilataC <ctalin9898@gmail.com>
2024-10-07 09:13:33 +0300

    chore: add time function utils

---
+++ src/utils/time.utils.ts
@@ -0,0 +1,12 @@
+export const DAY_IN_MS = 24 * 60 * 60 * 1000;
+// working with time can be complicated (timezones, daylight savings etc), hence making the followin simplifications
+// anytime during last 24h means "today"
+// anytime during last 48h which is not "today" means "yesterday"
+
+export const isToday = (currentMoment: number, timestamp: number) => currentMoment - timestamp < DAY_IN_MS; // should check if it's from future
+
+export const isYesterday = (currentMoment: number, timestamp: number) => 
+        !isToday(currentMoment, timestamp) && currentMoment - timestamp <  2* DAY_IN_MS;
+
+export const isOlderThanYesterday = (currentMoment: number, timestamp: number) => 
+        currentMoment - timestamp > 2* DAY_IN_MS;
